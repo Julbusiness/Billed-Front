1e802d06240331609b36d0a0ddbc3e575777211f
Julbusiness <julien.dutruch@yahoo.fr>
2023-01-03 14:50:41 +0100

    fix bug hunt - dashboard, en tant qu'administrateur lorsque l'on deplie plusieurs listes on peux choisir sur quelle tickets on veut clicquer et les voirs sans bug

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -84,6 +84,13 @@ export default class {
     this.document = document
     this.onNavigate = onNavigate
     this.store = store
+
+    this.tabsState = {
+      isOpen1: false,
+      isOpen2: false,
+      isOpen3: false,
+    }
+
     $('#arrow-icon1').click((e) => this.handleShowTickets(e, bills, 1))
     $('#arrow-icon2').click((e) => this.handleShowTickets(e, bills, 2))
     $('#arrow-icon3').click((e) => this.handleShowTickets(e, bills, 3))
@@ -111,7 +118,7 @@ export default class {
       $(`#open-bill${bill.id}`).css({ background: '#2A2B35' })
       $('.dashboard-right-container div').html(DashboardFormUI(bill))
       $('.vertical-navbar').css({ height: '150vh' })
-      this.counter ++
+      this.counter++
     } else {
       $(`#open-bill${bill.id}`).css({ background: '#0D5AE5' })
 
@@ -119,7 +126,7 @@ export default class {
         <div id="big-billed-icon" data-testid="big-billed-icon"> ${BigBilledIcon} </div>
       `)
       $('.vertical-navbar').css({ height: '120vh' })
-      this.counter ++
+      this.counter++
     }
     $('#icon-eye-d').click(this.handleClickIconEye)
     $('#btn-accept-bill').click((e) => this.handleAcceptSubmit(e, bill))
@@ -153,27 +160,42 @@ export default class {
   handleShowTickets(e, bills, index) {
     console.log('Je suis dans la fonction handleShowTickets dans le fichier Dashboard.js') //!
 
-    if (this.counter === undefined || this.index !== index) this.counter = 0
+    // if (this.counter === undefined || this.index !== index) this.counter = 0
+
     if (this.index === undefined || this.index !== index) this.index = index
-    if (this.counter % 2 === 0) {
+
+    //! test fix bug hunt - dashboard
+    if (!this.tabsState[`isOpen${index}`]) {
+      console.log("expanse")
+      console.log(index)
+      //console.log(this.tabsState)
+      //
       $(`#arrow-icon${this.index}`).css({ transform: 'rotate(0deg)'})
       $(`#status-bills-container${this.index}`)
         .html(cards(filteredBills(bills, getStatus(this.index))))
-      this.counter ++
-    } else {
+      this.tabsState[`isOpen${index}`] = true;
+
+    } else if (this.tabsState[`isOpen${index}`]){
+      console.log("collapse")
+      console.log(index)
+
       $(`#arrow-icon${this.index}`).css({ transform: 'rotate(90deg)'})
       $(`#status-bills-container${this.index}`)
         .html("")
-      this.counter ++
+      this.tabsState[`isOpen${index}`] = false;
+
     }
 
     bills.forEach(bill => {
+      $(`#open-bill${bill.id}`).off('click')
       $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
     })
 
     return bills
 
   }
+
+  //! fin du test
 
   getBillsAllUsers = () => {
     console.log('Je suis dans la fonction getBillsAllUsers dans le fichier Dashboard.js') //!
